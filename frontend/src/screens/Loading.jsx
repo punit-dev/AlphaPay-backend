0cfe74f019f5454f275b625747c9e6de87195548
punit-dev <punitpoddar2310@gmail.com>
2025-12-08 13:51:01 +0530

    Remove unused components, assets, and styles from the frontend, including App, Auth, Home, and various UI components. Delete SVG logos and configuration files to streamline the project structure.

--- frontend/src/screens/Loading.jsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <div className="h-screen w-full flex justify-center items-center bg-[#0000008d] backdrop-blur-md absolute top-0 left-0 z-10">
-      <div className="h-10 w-10 rounded-full border-2 border-x-white border-t-white animate-spin"></div>
-    </div>
-  );
-};
-
-export default Loading;
